d5c8b13e539feb1214bf37009a56ac9d45b88d28
Vasja Volin <vasja@superprofundo.com>
2010-11-06 15:33:28 -0400

    Setup Basic Page Structure

---
+++ userpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Your title here</title>
+<link rel="stylesheet" type="text/css" href="http://960ls.atomidata.com/static/cssserve/1/1/6/6/7/5/116675.css">
+<link rel="stylesheet" type="text/css" href="reset.css">
+</head>
+<body>
+                
+
+<div id="header" class="container_12">
+
+<div id="Logo" class="grid_3 ">
+</div>
+
+<div id="linkbar" class="grid_7 ">
+</div>
+
+<div id="loginbar" class="grid_2 ">
+</div>
+
+</div>
+
+                
+
+<div id="content" class="container_12">
+
+<div id="collageinfo" class="grid_6 ">
+</div>
+
+<div id="socialmeta" class="grid_6 ">
+</div>
+
+</div>
+
+                
+
+<div id="footer" class="container_12">
+
+<div id="meta" class="grid_12 ">
+</div>
+
+</div>
+
+            
+</body>
+</html>
